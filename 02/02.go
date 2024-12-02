188feccb7c2cad0f531ca6cffb27c7d8ca37a713
Blake Johnson <blake.k.johnson.4@gmail.com>
2024-12-02 18:46:35 -0500

    complete 02 part 2

--- 02/02.go
+++ 02/02.go
@@ -23,6 +23,23 @@ func main() {
     }
   }
   fmt.Printf("Safe Count: %d\n", safe_count)
+
+  safe_count_with_dampener := 0
+  for _, report := range reports {
+    report_safe := is_report_safe(report)
+    if report_safe {
+      safe_count_with_dampener += 1
+    } else {
+      // Yuck!
+      for index := range report.levels {
+        if is_report_safe(remove_level_from_report(report, index)) {
+          safe_count_with_dampener += 1
+          break
+        }
+      }
+    }
+  }
+  fmt.Printf("Safe Count with Dampener: %d\n", safe_count_with_dampener)
 }
 
 func is_report_safe(report report) bool {
@@ -33,20 +50,35 @@ func is_report_safe(report report) bool {
     diff := level - report.levels[index - 1]
     abs_diff := int(math.Abs(float64(diff)))
 
+    // Case where the difference is too large
     if abs_diff < 1 || abs_diff > 3 {
       return false
     }
 
     dir := diff / abs_diff
 
-    if len(dirs) > 0 && dir != dirs[len(dirs) - 1] {
-      return false
+    // Case where the direction changes
+    if len(dirs) > 0 && dirs[len(dirs) - 1] != 0 && dir != dirs[len(dirs) - 1] {
+        return false
     }
 
     dirs = append(dirs, dir)
   }
 
   return true
+}
+
+func remove_level_from_report(original_report report, level_index int) report {
+  new_report := report{
+    levels: []int{},
+  }
+
+  for index, level := range original_report.levels {
+    if index == level_index { continue }
+    new_report.levels = append(new_report.levels, level)
+  }
+
+  return new_report
 }
 
 func read_input() []report {
